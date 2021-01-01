b48adc2ad076b957d62dc412b6fda08e7fa775a7
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-01-01 16:47:12 +0200

    Add @Bean to MessageConverter

--- src/main/java/com/example/jms/config/JmsConfig.java
+++ src/main/java/com/example/jms/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.example.jms.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
+    @Bean
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
