41476ffcd38937127d99e17235740f95797ed1a8
Vitaliy Kalyniuk <kvit89@gmail.com>
2021-01-01 17:03:15 +0200

    Sent message

---
+++ src/main/java/com/example/jms/sender/HelloSender.java
@@ -0,0 +1,32 @@
+package com.example.jms.sender;
+
+import com.example.jms.config.JmsConfig;
+import com.example.jms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("Sending message");
+
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello world!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+
+        System.out.println("Message sent");
+    }
+}
